3e0f942e991526d78fbc3ccce348a2ac45ceb4ac
Greatness Prodigy <49532326+thegrtnx@users.noreply.github.com>
2023-03-30 03:11:25 +0100

    error page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './css/style.css';
 import Footer from './footer';
 import Homepage from './home';
+import Error404 from './error404';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <Homepage/>
-      <Footer/>
+    
+      <div className="content">
+        <Routes>
+          <Route path='/' element={<Homepage/>} />
+          <Route path='*' element={<Error404 />} />
+        </Routes>
+        <Footer/>
+      </div>
+     
     </div>
+  </Router>
   );
 }
 
